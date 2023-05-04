bcd8f2e85040997b20b2f68282a7d3f144bcb90e
AenR <40569269+AenR@users.noreply.github.com>
2023-05-05 01:05:51 +0300

    upload

---
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Play()
+    {
+
+    }
+
+}
