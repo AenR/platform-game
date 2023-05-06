e3e2ae0da1ef7cc7962cca00b74be535f957972a
AenR <40569269+AenR@users.noreply.github.com>
2023-05-07 02:16:27 +0300

    upload

--- Assets/Scripts/MenuScripts/PlayMenu.cs
+++ Assets/Scripts/MenuScripts/PlayMenu.cs
@@ -8,8 +8,6 @@ public class PlayMenu : MonoBehaviour
 
     public void Back()
     {
-        //PlayerPrefs.SetInt("SavedScene", SceneManager.GetActiveScene().buildIndex); son oynanan scene'in id'sini aliyor.
-        //karakter 2.bolumdeki trigger'a girince ustteki kod calistirilacak yani yeni bolum kaydedilecek
         SceneManager.LoadScene("MainMenu");
     }
 
